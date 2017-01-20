1f64cdb870daffc2724e1bcd4b8115e0593887af
BMeu <bastian@bastianmeyer.eu>
2017-01-20 10:57:23 +0100

    Load the social graph from a file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
-pub fn greet() -> String {
-    "Hello, World!".to_string()
+//! Reconstruct a retweet cascade.
+
+#![warn(missing_docs)]
+
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::path::Path;
+
+/// An edge in a graph.
+pub type Edge<T> = (T, T);
+
+/// Expected lines: user_id:friend1_id,friend2_id,... (IDs are integers).
+pub fn load_social_network_from_file<P>(filename: P) -> HashSet<Edge<u64>>
+    where P: AsRef<Path> {
+    let file = File::open(filename).expect("Could not open file.");
+    let file = BufReader::new(file);
+
+    let users: Vec<String> = file.lines()
+        .map(|line| line.expect("Error"))
+        .collect();
+
+    let mut friendships: HashSet<Edge<u64>> = HashSet::new();
+    for user in users {
+        let user: Vec<&str> = user.split(':').collect();
+        if user.len() == 0 {
+            continue;
+        }
+
+        let user_id: u64 = user[0].parse().unwrap();
+
+        let has_friends = user.len() > 1 && !user[1].is_empty();
+        if has_friends {
+            let friends: Vec<u64> = user[1].split(',')
+                .map(|friend| friend.parse().unwrap())
+                .collect();
+
+            for friend_id in friends {
+                let friendship: Edge<u64> = (user_id, friend_id);
+                friendships.insert(friendship);
+            }
+        }
+    }
+
+    friendships
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 extern crate cascadereconstruction_graphparallel;
+extern crate stopwatch;
+
+use stopwatch::Stopwatch;
 
 use cascadereconstruction_graphparallel::*;
 
 fn main() {
-    println!("{}", greet());
+    let stopwatch = Stopwatch::start_new();
+
+    let friendships = load_social_network_from_file("data/friends.txt");
+
+    println!("Time to load social network: {}", stopwatch);
+    println!("#Friendships: {}", friendships.len());
 }
