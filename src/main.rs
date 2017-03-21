c8bcbb019cb18e3a9704411e6823509473f1bd2d
BMeu <bastian@bastianmeyer.eu>
2017-03-21 16:30:42 +0100

    Better command line argument handling.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 extern crate crgp;
 extern crate fine_grained;
+extern crate getopts;
 extern crate serde_json;
 extern crate timely;
 
+use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::process;
 
 use fine_grained::Stopwatch;
+use getopts::{Matches, Options};
 use timely::dataflow::*;
 use timely::dataflow::operators::*;
 
@@ -16,19 +20,111 @@ use crgp::timely_extensions::Sync;
 use crgp::timely_extensions::operators::Reconstruct;
 use crgp::twitter::*;
 
+/// The exit codes returned by the program.
+pub enum ExitCode {
+    /// Successful (i.e. expected) execution (Code: `0`).
+    Success = 0,
+
+    /// Invalid program parameters (Code: `1`).
+    IncorrectUsage = 1
+}
+
+/// Execute the program.
 fn main() {
-    let name = &std::env::args().nth(0).unwrap();
-    println!("Usage: {} <Friend Data Set: Path> <Retweet Data Set: Path> <Batch Size: Int> <Print Result: Bool> [Timely Options]", name);
-    println!();
+    let arguments: Vec<String> = env::args().collect();
+    let program: &str = &arguments[0].clone();
 
-    // Parse the arguments.
-    let friendship_dataset = std::env::args().nth(1).unwrap();
-    let retweet_dataset = std::env::args().nth(2).unwrap();
-    let batch_size: usize = std::env::args().nth(3).unwrap().parse().unwrap();
-    let print_result: bool = std::env::args().nth(4).unwrap().parse().unwrap();
-    let timely_args = std::env::args().skip(4);
+    // Define defaults.
+    let default_batch_size: usize = 500;
 
-    execute(friendship_dataset, retweet_dataset, batch_size, print_result, timely_args);
+    // Define the program options.
+    let mut options = Options::new();
+    options.optopt("b", "batch-size",
+                   &format!("Size of retweet batches. [Default: {default}]",
+                            default = default_batch_size),
+                   "SIZE");
+    options.optopt("f", "hostfile", "A text file specifying \"hostname:port\" per line in order of process identity.", "FILE");
+    options.optflag("h", "help", "Show this usage message and exit.");
+    options.optopt("n", "processes", "Number of processes involved in the computation.", "PROCESSES");
+    options.optopt("p", "process", "Identity of this process; from 0 to n-1.", "ID");
+    options.optflag("r", "print-result", "Print the computed influence edges to stdout.");
+    options.optopt("w", "workers", "Number of per-process workers threads.", "WORKERS");
+
+    // Parse the options.
+    let specified_options: Matches = match options.parse(&arguments[1..]) {
+        Ok(option_matches) => option_matches,
+        Err(message) => {
+            println!("Error: {error}", error = message.to_string());
+            print_usage(program, options);
+            process::exit(ExitCode::IncorrectUsage as i32);
+        }
+    };
+
+    // If the help option is specified, show the usage information and exit the program.
+    if specified_options.opt_present("h") {
+        print_usage(program, options);
+        process::exit(ExitCode::Success as i32);
+    }
+
+    // Get the parsed options.
+    let print_result: bool = specified_options.opt_present("r");
+    let batch_size: usize = match specified_options.opt_str("b") {
+        Some(batch_size) => {
+            match batch_size.parse::<usize>() {
+                Ok(batch_size) => batch_size,
+                Err(message) => {
+                    println!("Error: {error}", error = message.to_string());
+                    print_usage(&program, options);
+                    process::exit(ExitCode::IncorrectUsage as i32);
+                }
+            }
+        },
+        None => default_batch_size
+    };
+
+    // Create the arguments for the timely execution.
+    let mut timely_arguments: Vec<String> = Vec::new();
+    if let Some(hostfile) = specified_options.opt_str("f") {
+        timely_arguments.push("-h".to_string());
+        timely_arguments.push(hostfile.to_string());
+    }
+    if let Some(processes) = specified_options.opt_str("n") {
+        timely_arguments.push("-n".to_string());
+        timely_arguments.push(processes.to_string());
+    }
+    if let Some(process) = specified_options.opt_str("p") {
+        timely_arguments.push("-p".to_string());
+        timely_arguments.push(process.to_string());
+    }
+    if let Some(workers) = specified_options.opt_str("w") {
+        timely_arguments.push("-w".to_string());
+        timely_arguments.push(workers.to_string());
+    }
+    let timely_arguments = timely_arguments.into_iter();
+
+    // Get the paths to the data sets.
+    if specified_options.free.len() < 2 {
+        let message: &str = match specified_options.free.len() {
+            0 => "Neither friends data set nor retweet data set are given.",
+            1 => "No retweet data set is given.",
+            _ => ""
+        };
+        println!("Error: {error}", error = message.to_string());
+        print_usage(program, options);
+        process::exit(ExitCode::IncorrectUsage as i32);
+    }
+    let friendship_dataset: String = specified_options.free[0].clone();
+    let retweet_dataset: String = specified_options.free[1].clone();
+
+    // Execute the reconstruction.
+    execute(friendship_dataset, retweet_dataset, batch_size, print_result, timely_arguments);
+    process::exit(ExitCode::Success as i32);
+}
+
+/// Print a usage message.
+fn print_usage(program: &str, options: Options) {
+    let brief = &format!("Usage: {executable} <options> [FRIENDS] [RETWEETS]", executable = program);
+    println!("{}", options.usage(brief));
 }
 
 /// Execute the reconstruction.
@@ -162,13 +258,19 @@ fn execute<I>(friendship_dataset: String, retweet_dataset: String, batch_size: u
             println!("  #Retweets: {}", number_of_retweets);
             println!("  Batch Size: {}", batch_size);
             println!();
-            println!("  Time to set up the computation: {:.3}ms", time_to_setup as f64 / 1_000_000.0f64);
-            println!("  Time to load and process the social network: {:.3}ms", time_to_process_social_network as f64 / 1_000_000.0f64);
-            println!("  Time to load the retweets: {:.3}ms", time_to_load_retweets as f64 / 1_000_000.0f64);
-            println!("  Time to process the retweets: {:.3}ms", time_to_process_retweets as f64 / 1_000_000.0f64);
-            println!("  Total time: {:.3}ms", stopwatch.total_time() as f64 / 1_000_000.0f64);
+            println!("  Time to set up the computation: {:.3}ms",
+                     time_to_setup as f64 / 1_000_000.0f64);
+            println!("  Time to load and process the social network: {:.3}ms",
+                     time_to_process_social_network as f64 / 1_000_000.0f64);
+            println!("  Time to load the retweets: {:.3}ms",
+                     time_to_load_retweets as f64 / 1_000_000.0f64);
+            println!("  Time to process the retweets: {:.3}ms",
+                     time_to_process_retweets as f64 / 1_000_000.0f64);
+            println!("  Total time: {:.3}ms",
+                     stopwatch.total_time() as f64 / 1_000_000.0f64);
             println!();
-            println!("  Retweet Processing Rate: {:.3} RT/s", number_of_retweets as f64 / (time_to_process_retweets as f64 / 1_000_000_000.0f64))
+            println!("  Retweet Processing Rate: {:.3} RT/s",
+                     number_of_retweets as f64 / (time_to_process_retweets as f64 / 1_000_000_000.0f64))
         }
     }).unwrap();
 }
