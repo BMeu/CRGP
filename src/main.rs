f4af90fa1cd631610939da9969f13db2bd89eda2
BMeu <bastian@bastianmeyer.eu>
2017-03-22 09:33:52 +0100

    Switch to clap instead of getopts to parse CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 //! Execute the graph-parallel cascade reconstruction from the command line.
 
+#[macro_use]
+extern crate clap;
 extern crate crgp;
-extern crate getopts;
 
-use std::env;
 use std::process;
 
-use getopts::{Matches, Options};
+use clap::{Arg, ArgMatches};
 
 use crgp::algorithm;
 
@@ -21,97 +21,97 @@ pub enum ExitCode {
 
 /// Execute the program.
 fn main() {
-    let arguments: Vec<String> = env::args().collect();
-    let program: &str = &arguments[0].clone();
+    // Define the usage.
+    let arguments: ArgMatches = app_from_crate!()
+        .arg(Arg::with_name("batch-size")
+            .short("b")
+            .long("batch-size")
+            .value_name("SIZE")
+            .help("Size of retweet batches")
+            .takes_value(true)
+            .default_value("500")
+            .validator(|value: String| -> Result<(), String> {
+                // The batch size must be a positive integer.
+                let is_valid = match value.parse::<usize>() {
+                    Ok(value) => value > 0,
+                    Err(_) => false
+                };
 
-    // Define defaults.
-    let default_batch_size: usize = 500;
-
-    // Define the program options.
-    let mut options = Options::new();
-    options.optopt("b", "batch-size",
-                   &format!("Size of retweet batches. [Default: {default}]", default = default_batch_size), "SIZE");
-    options.optopt("f", "hostfile", "A text file specifying \"hostname:port\" per line in order of process identity.",
-                   "FILE");
-    options.optflag("h", "help", "Show this usage message and exit.");
-    options.optopt("n", "processes", "Number of processes involved in the computation.", "PROCESSES");
-    options.optopt("p", "process", "Identity of this process; from 0 to n-1.", "ID");
-    options.optflag("r", "print-result", "Print the computed influence edges to stdout.");
-    options.optopt("w", "workers", "Number of per-process workers threads.", "WORKERS");
-
-    // Parse the options.
-    let specified_options: Matches = match options.parse(&arguments[1..]) {
-        Ok(option_matches) => option_matches,
-        Err(message) => {
-            println!("Error: {error}", error = message.to_string());
-            print_usage(program, options);
-            process::exit(ExitCode::IncorrectUsage as i32);
-        }
-    };
-
-    // If the help option is specified, show the usage information and exit the program.
-    if specified_options.opt_present("h") {
-        print_usage(program, options);
-        process::exit(ExitCode::Success as i32);
-    }
-
-    // Get the parsed options.
-    let print_result: bool = specified_options.opt_present("r");
-    let batch_size: usize = match specified_options.opt_str("b") {
-        Some(batch_size) => {
-            match batch_size.parse::<usize>() {
-                Ok(batch_size) => batch_size,
-                Err(message) => {
-                    println!("Error: {error}", error = message.to_string());
-                    print_usage(&program, options);
-                    process::exit(ExitCode::IncorrectUsage as i32);
+                if !is_valid {
+                    Err(String::from("The batch size must be a positive integer."))
                 }
-            }
-        },
-        None => default_batch_size
-    };
+                else {
+                    Ok(())
+                }
+            }))
+        .arg(Arg::with_name("hostfile")
+            .short("f")
+            .long("hostfile")
+            .value_name("FILE")
+            .help("A text file specifying \"hostname:port\" per line in order of process identity")
+            .takes_value(true))
+        .arg(Arg::with_name("processes")
+            .short("n")
+            .long("processes")
+            .value_name("PROCESSES")
+            .help("Number of processes involved in the computation")
+            .takes_value(true))
+        .arg(Arg::with_name("process")
+            .short("p")
+            .long("process")
+            .value_name("ID")
+            .help("Identity of this process; from 0 to n-1")
+            .takes_value(true))
+        .arg(Arg::with_name("print-results")
+            .short("r")
+            .long("print-result")
+            .help("Prints the computed influence edges to STDOUT"))
+        .arg(Arg::with_name("workers")
+            .short("w")
+            .long("workers")
+            .value_name("WORKERS")
+            .help("Number of per-process worker threads")
+            .takes_value(true))
+        .arg(Arg::with_name("FRIENDS")
+            .help("Path to the friendship dataset")
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("RETWEETS")
+            .help("Path to the Retweet dataset")
+            .required(true)
+            .index(2))
+        .get_matches();
+
+    // Get the arguments. Since these arguments have default values and validators defined none of the `unwrap()`s
+    // can fail.
+    let batch_size: usize = arguments.value_of("batch-size").unwrap().parse().unwrap();
+    let print_result: bool = arguments.is_present("print-results");
+
+    // Get the positional arguments. Since they are required the `unwrap()`s cannot fail.
+    let friendship_dataset: String = arguments.value_of("FRIENDS").unwrap().to_owned();
+    let retweet_dataset: String = arguments.value_of("RETWEETS").unwrap().to_owned();
 
     // Create the arguments for the timely execution.
     let mut timely_arguments: Vec<String> = Vec::new();
-    if let Some(hostfile) = specified_options.opt_str("f") {
-        timely_arguments.push("-h".to_string());
-        timely_arguments.push(hostfile.to_string());
+    if let Some(hostfile) = arguments.value_of("hostfile") {
+        timely_arguments.push("-h".to_owned());
+        timely_arguments.push(hostfile.to_owned());
     }
-    if let Some(processes) = specified_options.opt_str("n") {
-        timely_arguments.push("-n".to_string());
-        timely_arguments.push(processes.to_string());
+    if let Some(processes) = arguments.value_of("processes") {
+        timely_arguments.push("-n".to_owned());
+        timely_arguments.push(processes.to_owned());
     }
-    if let Some(process) = specified_options.opt_str("p") {
-        timely_arguments.push("-p".to_string());
-        timely_arguments.push(process.to_string());
+    if let Some(process) = arguments.value_of("process") {
+        timely_arguments.push("-p".to_owned());
+        timely_arguments.push(process.to_owned());
     }
-    if let Some(workers) = specified_options.opt_str("w") {
-        timely_arguments.push("-w".to_string());
-        timely_arguments.push(workers.to_string());
+    if let Some(workers) = arguments.value_of("workers") {
+        timely_arguments.push("-w".to_owned());
+        timely_arguments.push(workers.to_owned());
     }
-    let timely_arguments = timely_arguments.into_iter();
-
-    // Get the paths to the data sets.
-    if specified_options.free.len() < 2 {
-        let message: &str = match specified_options.free.len() {
-            0 => "Neither friends data set nor retweet data set are given.",
-            1 => "No retweet data set is given.",
-            _ => ""
-        };
-        println!("Error: {error}", error = message.to_string());
-        print_usage(program, options);
-        process::exit(ExitCode::IncorrectUsage as i32);
-    }
-    let friendship_dataset: String = specified_options.free[0].clone();
-    let retweet_dataset: String = specified_options.free[1].clone();
+    let timely_arguments: std::vec::IntoIter<String> = timely_arguments.into_iter();
 
     // Execute the reconstruction.
     algorithm::execute(friendship_dataset, retweet_dataset, batch_size, print_result, timely_arguments);
     process::exit(ExitCode::Success as i32);
-}
-
-/// Print a usage message.
-fn print_usage(program: &str, options: Options) {
-    let brief = &format!("Usage: {executable} <options> [FRIENDS] [RETWEETS]", executable = program);
-    println!("{}", options.usage(brief));
 }
