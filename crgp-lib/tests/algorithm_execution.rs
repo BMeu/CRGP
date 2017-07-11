5664835c59c735d64e6ff4294915217785b8a9e0
BMeu <bastian@bastianmeyer.eu>
2017-07-11 14:20:38 +0200

    Do not hard code paths

--- crgp-lib/tests/algorithm_execution.rs
+++ crgp-lib/tests/algorithm_execution.rs
@@ -5,6 +5,7 @@
 // modified, or distributed except according to those terms.
 
 extern crate crgp_lib;
+extern crate find_folder;
 #[cfg(unix)]
 extern crate gag;
 #[cfg(unix)]
@@ -16,6 +17,7 @@ use std::io::Read;
 use std::path::PathBuf;
 use std::sync::Mutex;
 
+use find_folder::Search;
 #[cfg(unix)]
 use gag::BufferRedirect;
 
@@ -32,8 +34,10 @@ lazy_static! {
 
 #[test]
 fn algorithm_execution_gale() {
-    let friendship_dataset = InputSource::new("../data/social_graph");
-    let retweet_dataset = InputSource::new("../data/retweets.json");
+    let data_path: PathBuf = Search::ParentsThenKids(3, 3).for_folder("data").expect("Data folder not found.");
+
+    let friendship_dataset = InputSource::new(data_path.join("social_graph").to_str().unwrap());
+    let retweet_dataset = InputSource::new(data_path.join("retweets.json").to_str().unwrap());
 
     let configuration = Configuration::default(retweet_dataset, friendship_dataset)
         .batch_size(1);
@@ -76,9 +80,11 @@ fn algorithm_execution_gale() {
 
 #[test]
 fn algorithm_execution_gale_with_selected_users() {
-    let friendship_dataset = InputSource::new("../data/social_graph");
-    let retweet_dataset = InputSource::new("../data/retweets.json");
-    let selected_users = PathBuf::from("../data/retweeting_users.txt");
+    let data_path: PathBuf = Search::ParentsThenKids(3, 3).for_folder("data").expect("Data folder not found.");
+
+    let friendship_dataset = InputSource::new(data_path.join("social_graph").to_str().unwrap());
+    let retweet_dataset = InputSource::new(data_path.join("retweets.json").to_str().unwrap());
+    let selected_users: PathBuf = data_path.join("retweeting_users.txt");
 
     let configuration = Configuration::default(retweet_dataset, friendship_dataset)
         .batch_size(1)
@@ -122,8 +128,10 @@ fn algorithm_execution_gale_with_selected_users() {
 
 #[test]
 fn algorithm_execution_gale_with_dummy_users() {
-    let friendship_dataset = InputSource::new("../data/social_graph");
-    let retweet_dataset = InputSource::new("../data/retweets.json");
+    let data_path: PathBuf = Search::ParentsThenKids(3, 3).for_folder("data").expect("Data folder not found.");
+
+    let friendship_dataset = InputSource::new(data_path.join("social_graph").to_str().unwrap());
+    let retweet_dataset = InputSource::new(data_path.join("retweets.json").to_str().unwrap());
 
     let configuration = Configuration::default(retweet_dataset, friendship_dataset)
         .batch_size(1)
@@ -167,9 +175,11 @@ fn algorithm_execution_gale_with_dummy_users() {
 
 #[test]
 fn algorithm_execution_gale_with_selected_users_and_dummy_friends() {
-    let friendship_dataset = InputSource::new("../data/social_graph");
-    let retweet_dataset = InputSource::new("../data/retweets.json");
-    let selected_users = PathBuf::from("../data/retweeting_users.txt");
+    let data_path: PathBuf = Search::ParentsThenKids(3, 3).for_folder("data").expect("Data folder not found.");
+
+    let friendship_dataset = InputSource::new(data_path.join("social_graph").to_str().unwrap());
+    let retweet_dataset = InputSource::new(data_path.join("retweets.json").to_str().unwrap());
+    let selected_users: PathBuf = data_path.join("retweeting_users.txt");
 
     let configuration = Configuration::default(retweet_dataset, friendship_dataset)
         .batch_size(1)
@@ -214,8 +224,10 @@ fn algorithm_execution_gale_with_selected_users_and_dummy_friends() {
 
 #[test]
 fn algorithm_execution_leaf() {
-    let friendship_dataset = InputSource::new("../data/social_graph");
-    let retweet_dataset = InputSource::new("../data/retweets.json");
+    let data_path: PathBuf = Search::ParentsThenKids(3, 3).for_folder("data").expect("Data folder not found.");
+
+    let friendship_dataset = InputSource::new(data_path.join("social_graph").to_str().unwrap());
+    let retweet_dataset = InputSource::new(data_path.join("retweets.json").to_str().unwrap());
 
     let configuration = Configuration::default(retweet_dataset, friendship_dataset)
         .algorithm(Algorithm::LEAF)
@@ -259,9 +271,11 @@ fn algorithm_execution_leaf() {
 
 #[test]
 fn algorithm_execution_leaf_with_selected_users() {
-    let friendship_dataset = InputSource::new("../data/social_graph");
-    let retweet_dataset = InputSource::new("../data/retweets.json");
-    let selected_users = PathBuf::from("../data/retweeting_users.txt");
+    let data_path: PathBuf = Search::ParentsThenKids(3, 3).for_folder("data").expect("Data folder not found.");
+
+    let friendship_dataset = InputSource::new(data_path.join("social_graph").to_str().unwrap());
+    let retweet_dataset = InputSource::new(data_path.join("retweets.json").to_str().unwrap());
+    let selected_users: PathBuf = data_path.join("retweeting_users.txt");
 
     let configuration = Configuration::default(retweet_dataset, friendship_dataset)
         .algorithm(Algorithm::LEAF)
@@ -306,8 +320,10 @@ fn algorithm_execution_leaf_with_selected_users() {
 
 #[test]
 fn algorithm_execution_leaf_with_dummy_users() {
-    let friendship_dataset = InputSource::new("../data/social_graph");
-    let retweet_dataset = InputSource::new("../data/retweets.json");
+    let data_path: PathBuf = Search::ParentsThenKids(3, 3).for_folder("data").expect("Data folder not found.");
+
+    let friendship_dataset = InputSource::new(data_path.join("social_graph").to_str().unwrap());
+    let retweet_dataset = InputSource::new(data_path.join("retweets.json").to_str().unwrap());
 
     let configuration = Configuration::default(retweet_dataset, friendship_dataset)
         .algorithm(Algorithm::LEAF)
@@ -352,9 +368,11 @@ fn algorithm_execution_leaf_with_dummy_users() {
 
 #[test]
 fn algorithm_execution_leaf_with_selected_users_and_dummy_friends() {
-    let friendship_dataset = InputSource::new("../data/social_graph");
-    let retweet_dataset = InputSource::new("../data/retweets.json");
-    let selected_users = PathBuf::from("../data/retweeting_users.txt");
+    let data_path: PathBuf = Search::ParentsThenKids(3, 3).for_folder("data").expect("Data folder not found.");
+
+    let friendship_dataset = InputSource::new(data_path.join("social_graph").to_str().unwrap());
+    let retweet_dataset = InputSource::new(data_path.join("retweets.json").to_str().unwrap());
+    let selected_users: PathBuf = data_path.join("retweeting_users.txt");
 
     let configuration = Configuration::default(retweet_dataset, friendship_dataset)
         .algorithm(Algorithm::LEAF)
